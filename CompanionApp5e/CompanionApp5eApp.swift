9e851658b205e83e5af20a3145f79068f917ef47
orleonedev <orleone.dev@gmail.com>
2021-12-10 16:04:55 +0100

    Initial Commit

---
+++ CompanionApp5e/CompanionApp5eApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompanionApp5eApp.swift
+//  CompanionApp5e
+//
+//  Created by Oreste Leone on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CompanionApp5eApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CompanionApp5e/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CompanionApp5e
+//
+//  Created by Oreste Leone on 10/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
